f7db71e6a48f8d8820853d272112d625da2db3fe
AlbertGrn <albert.scherf@web.de>
2022-09-11 15:37:32 +0200

    Implemented NavBar
    - Not functional (just design)
    - Not responsive

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,13 @@
-function init() {
-    console.log("### Starting MME Project ###"); // eslint-disable-line no-console
-}
+let NAV_LINKS = document.getElementsByClassName("nav-link")
 
-init();
+
+for (let item of NAV_LINKS) {
+    item.addEventListener("click", (event) => {
+        for (let item of NAV_LINKS) {
+            if (item.classList.contains("active")) {
+                item.classList.remove("active")
+            }
+        }
+        item.classList.add("active")
+    })
+}
