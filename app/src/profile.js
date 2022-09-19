69168338647ce06a80dbc6b1eea718cbfda91341
AlbertGrn <albert.scherf@web.de>
2022-09-19 13:39:08 +0200

    Added functionality to settings nav bar

--- app/src/profile.js
+++ app/src/profile.js
@@ -1,4 +1,4 @@
-const SETTINGS_NAV_LINKS = document.getElementsByClassName("nav-link");
+const SETTINGS_NAV_LINKS = document.querySelectorAll("#settings-nav .nav-link");
 const SETTINGS_FRAME = document.querySelector(".settings-frame");
 
 const BANNED_INGREDIENTS_SECTION_TEMPLATE = document.querySelector("#banned-ingredients-section-template").innerHTML.trim();
@@ -13,17 +13,32 @@ function getViewFromTemplate(template) {
 for (let item of SETTINGS_NAV_LINKS) {
     item.addEventListener("click", (event) => {
         if(item.id == "favorites") {
+            SETTINGS_FRAME.innerHTML = "";
+
             console.log("open favorites");
         }
         else if(item.id == "banned-ingredients") {
+            SETTINGS_FRAME.innerHTML = "";
+
             console.log("open banned-ingredients");
             SETTINGS_FRAME.append(getViewFromTemplate(BANNED_INGREDIENTS_SECTION_TEMPLATE));
         }
         else if(item.id == "created-cocktails") {
+            SETTINGS_FRAME.innerHTML = "";
+
             console.log("open created-cocktails");
         }
         else {
+            SETTINGS_FRAME.innerHTML = "";
+            
             console.log("open reviews");
         }
+
+        for (let item of SETTINGS_NAV_LINKS) {
+            if (item.classList.contains("active")) {
+                item.classList.remove("active")
+            }
+        }
+        item.classList.add("active")
     })
 }
