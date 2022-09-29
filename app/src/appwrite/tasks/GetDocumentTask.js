9feb0cac022b64c86b33268f072a8fe1edd96415
wai wha <87634748+waiwha-project@users.noreply.github.com>
2022-09-29 17:42:36 +0200

    forgot something

--- app/src/appwrite/tasks/GetDocumentTask.js
+++ app/src/appwrite/tasks/GetDocumentTask.js
@@ -6,11 +6,7 @@ export default class GetDocumentTask extends AppwriteTask {
   async createResult(id) {
     let database = new this.appwrite.Databases(this.client, Config.database);
     try {
-<<<<<<< Updated upstream
-      return await database.getDocument(Config.recipe, id); // Config.collections.recipe ??
-=======
       return await database.getDocument(Config.collections.recipe, id);
->>>>>>> Stashed changes
     } catch (error) {
       console.error(error);
       throw new Error("Error while trying to get a document.");
