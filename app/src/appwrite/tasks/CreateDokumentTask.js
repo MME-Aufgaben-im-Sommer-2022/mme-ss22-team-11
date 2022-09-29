0da855f08ac5a20b9b674c27630511c60a76d2c7
Alexander Bazo <alexanderbazo@googlemail.com>
2022-09-29 17:18:27 +0200

    Fix bug where no unique id was created for new document on server

--- app/src/appwrite/tasks/CreateDokumentTask.js
+++ app/src/appwrite/tasks/CreateDokumentTask.js
@@ -6,7 +6,7 @@ export default class CreateDocumentTask extends AppwriteTask {
   let database = new this.appwrite.Databases(this.client, input.database);
    try {
    // Let appwrite create a unique id for this new document
-   return await database.createDocument(input.collection, "unique", input.document);
+   return await database.createDocument(input.collection, "unique()", input.document);
   } catch (error) {
    console.log(error);
    throw new Error("Error while trying to create document on server");
