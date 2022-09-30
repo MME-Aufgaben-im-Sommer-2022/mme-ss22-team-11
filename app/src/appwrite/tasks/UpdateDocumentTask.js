8bd49206d60c210d27b5b9029864bde7c6d19888
Georg Dechant <86664255+GixD42@users.noreply.github.com>
2022-09-30 17:44:19 +0200

    fixed error in UpdateDocumentTask.js

--- app/src/appwrite/tasks/UpdateDocumentTask.js
+++ app/src/appwrite/tasks/UpdateDocumentTask.js
@@ -6,7 +6,7 @@ export default class UpdateDocumentTask extends AppwriteTask {
   async createResult(input) {
     let database = new this.appwrite.Databases(this.client, Config.database.id);
     try {
-      await database.updateDocument(Config.database.collections.recipe, input.id, input.data); 
+      await database.updateDocument(Config.database.collections.recipe.id, input.id, input.data); 
     } catch (error) {
       console.error(error);
       throw new Error("Error while trying to update a document.");
