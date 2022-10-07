a27557af051e1531d690fc7c8310647fae2080f9
wai wha <87634748+waiwha-project@users.noreply.github.com>
2022-10-07 19:25:09 +0200

    cocktailCreator.js eslint

--- app/src/cocktailData/cocktailCreator.js
+++ app/src/cocktailData/cocktailCreator.js
@@ -2,21 +2,12 @@ import { Observable, Event } from "../utils/Observable.js";
 import { CocktailListManager } from "./cocktailListManager.js";
 import { Recipe } from "./recipe.js";
 
-import { IngredientFilterManager } from "./ingredientFilterManager.js"
-import { CreatorIngredientListView } from "../ui/ingredients/CreatorIngredientListView.js"
-
-
-
+import { IngredientFilterManager } from "./ingredientFilterManager.js";
+import { CreatorIngredientListView } from "../ui/ingredients/CreatorIngredientListView.js";
 
 const COCKTAIL_CREATOR_TEMPLATE = document.getElementById("recipe-creator-section-template").innerHTML.trim();
 
-
 class CocktailCreator extends Observable {
-
-
-
-
-
 
     constructor() {
         super();
@@ -26,21 +17,21 @@ class CocktailCreator extends Observable {
 
         this.initializeCreator();
 
-        this.IMAGE_INPUT = document.querySelector(".image-input")
-        this.IMAGE_INPUT_LABEL = document.querySelector(".image-input-label")
-        this.CREATOR_TAG_INPUT_CONTAINER = document.querySelector(".creator-tag-input-container")
-        this.CREATOR_ING_CONTAINER = document.querySelector(".creator-ingredients-container")
-        this.CREATOR_ING_INPUT_CONTAINER = document.querySelectorAll(".creator-ingredient-input-container")
-        this.CREATOR_ING_RESULTS = document.querySelector(".creator-ingredient-input-results")
-        this.CREATOR_ING_ADD = document.querySelector(".creator-ingredient-add")
-        this.CREATOR_ING_RESULT_CONTAINER = document.querySelectorAll(".creator-ingredient-input-results")
-        this.CREATOR_ING_TEMPLATE = document.querySelector("#creator-ingredient-template")
-        this.CREATOR_INS_CONTAINER = document.querySelector(".creator-instructions-container")
-        this.CREATOR_INS_INPUT_CONTAINER = document.querySelectorAll(".creator-instructions-input-container")
-        this.CREATOR_INS_ADD = document.querySelector(".creator-instructions-add")
-        this.CREATOR_ING_INPUT_CONTAINER_TEMPLATE = document.querySelector("#creator-ingredient-input-container-template").innerHTML.trim()
-        this.CREATOR_INS_INPUT_CONTAINER_TEMPLATE = document.querySelector("#creator-instructions-input-container-template").innerHTML.trim()
-        this.CREATOR_SUMBIT = document.querySelector(".submit-button")
+        this.IMAGE_INPUT = document.querySelector(".image-input");
+        this.IMAGE_INPUT_LABEL = document.querySelector(".image-input-label");
+        this.CREATOR_TAG_INPUT_CONTAINER = document.querySelector(".creator-tag-input-container");
+        this.CREATOR_ING_CONTAINER = document.querySelector(".creator-ingredients-container");
+        this.CREATOR_ING_INPUT_CONTAINER = document.querySelectorAll(".creator-ingredient-input-container");
+        this.CREATOR_ING_RESULTS = document.querySelector(".creator-ingredient-input-results");
+        this.CREATOR_ING_ADD = document.querySelector(".creator-ingredient-add");
+        this.CREATOR_ING_RESULT_CONTAINER = document.querySelectorAll(".creator-ingredient-input-results");
+        this.CREATOR_ING_TEMPLATE = document.querySelector("#creator-ingredient-template");
+        this.CREATOR_INS_CONTAINER = document.querySelector(".creator-instructions-container");
+        this.CREATOR_INS_INPUT_CONTAINER = document.querySelectorAll(".creator-instructions-input-container");
+        this.CREATOR_INS_ADD = document.querySelector(".creator-instructions-add");
+        this.CREATOR_ING_INPUT_CONTAINER_TEMPLATE = document.querySelector("#creator-ingredient-input-container-template").innerHTML.trim();
+        this.CREATOR_INS_INPUT_CONTAINER_TEMPLATE = document.querySelector("#creator-instructions-input-container-template").innerHTML.trim();
+        this.CREATOR_SUMBIT = document.querySelector(".submit-button");
 
         this.IMAGE_INPUT.addEventListener("change", (event) => this.validateFileType(event));
 
@@ -63,11 +54,11 @@ class CocktailCreator extends Observable {
         let el = document.createElement("div");
         el.innerHTML = COCKTAIL_CREATOR_TEMPLATE;
         el = el.querySelector(".recipe-creator-section");
-        el.querySelector(".creator-cancel").addEventListener("click", (event) => {
+        el.querySelector(".creator-cancel").addEventListener("click", () => {
             el.remove();
             document.querySelector("#filter").style.display = "flex";
             document.querySelector(".cocktails").style.display = "block";
-        })
+        });
         return el;
     }
 
@@ -76,11 +67,11 @@ class CocktailCreator extends Observable {
     */
 
     validateFileType(event) {
+        var fileName = this.IMAGE_INPUT.value,
+            idxDot = fileName.lastIndexOf(".") + 1,
+            extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
         this.file = event;
-        var fileName = this.IMAGE_INPUT.value;
-        var idxDot = fileName.lastIndexOf(".") + 1;
-        var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
-        if (extFile == "jpg" || extFile == "jpeg" || extFile == "png") {
+        if (extFile === "jpg" || extFile === "jpeg" || extFile === "png") {
 
             let getImagePath = URL.createObjectURL(event.target.files[0]);
             this.IMAGE_INPUT_LABEL.style.background = `url(${getImagePath}) center`;
@@ -105,7 +96,6 @@ class CocktailCreator extends Observable {
         return el.querySelector(".creator-instructions-input-container");
     }
 
-
     /*
         TAGS
     */
@@ -127,7 +117,7 @@ class CocktailCreator extends Observable {
                 } else if (maxTagCount > 0) {
                     tags[k].classList.add("selected");
                 }
-            })
+            });
         }
     }
 
@@ -136,8 +126,10 @@ class CocktailCreator extends Observable {
     */
 
     fillIngredientList(container, input) {
-        let ingredientFilterManager = new IngredientFilterManager();
-        let ingredientListView = new CreatorIngredientListView(container, input);
+        let ingredientFilterManager = new IngredientFilterManager(),
+            ingredientListView = new CreatorIngredientListView(container, input),
+            timeout = null,
+            responseDelay = 500;
 
         ingredientFilterManager.addEventListener("INGREDIENT_DATA_READY", () => {
             ingredientListView.refreshSearchResults(ingredientFilterManager.displayList);
@@ -148,10 +140,8 @@ class CocktailCreator extends Observable {
         });
 
         // search functionality
-        let timeout = null,
-            responseDelay = 500;
 
-        input.addEventListener('keyup', function () {
+        input.addEventListener("keyup", function () {
             clearTimeout(timeout);
             timeout = setTimeout(() => {
                 ingredientFilterManager.searchIngredientByName(input.value);
@@ -166,15 +156,15 @@ class CocktailCreator extends Observable {
 
     initializeIngredientInputContainer() {
         for (let i = 0; i < this.CREATOR_ING_INPUT_CONTAINER.length; i++) {
-            this.CREATOR_ING_INPUT_CONTAINER[i].addEventListener("click", (event) => {
-                if (this.CREATOR_ING_RESULTS.childElementCount != 0) {
+            this.CREATOR_ING_INPUT_CONTAINER[i].addEventListener("click", () => {
+                if (this.CREATOR_ING_RESULTS.childElementCount !== 0) {
                     this.CREATOR_ING_INPUT_CONTAINER[i].classList.add("extend");
                 } else {
                     this.CREATOR_ING_INPUT_CONTAINER[i].classList.add("focus");
                 }
             });
             document.addEventListener("click", (event) => {
-                if (!this.CREATOR_ING_INPUT_CONTAINER[i].contains(event.target) && event.target.className != "creator-ingredient") {
+                if (!this.CREATOR_ING_INPUT_CONTAINER[i].contains(event.target) && event.target.className !== "creator-ingredient") {
                     if (this.CREATOR_ING_INPUT_CONTAINER[i].classList.contains("focus")) {
                         this.CREATOR_ING_INPUT_CONTAINER[i].classList.remove("focus");
                     } else if (this.CREATOR_ING_INPUT_CONTAINER[i].classList.contains("extend")) {
@@ -189,34 +179,33 @@ class CocktailCreator extends Observable {
 
             this.fillIngredientList(this.CREATOR_ING_INPUT_CONTAINER[i].querySelector(".creator-ingredient-input-results"), this.CREATOR_ING_INPUT_CONTAINER[i].querySelector(".creator-ingredient-input"));
 
-
-            if (i != 0) {
-                if (this.CREATOR_ING_INPUT_CONTAINER[i].querySelector(".creator-ingredient-input").textContent == "") {
+            if (i !== 0) {
+                if (this.CREATOR_ING_INPUT_CONTAINER[i].querySelector(".creator-ingredient-input").textContent === "") {
                     this.CREATOR_ING_INPUT_CONTAINER[i].querySelector(".creator-ingredient-input").addEventListener("keydown", (event) => {
-                        console.log(event.keyCode);
-                        if (event.keyCode == 8) {
-                            console.log("DELETE");
+                        //console.log(event.keyCode);
+                        // eslint-disable-next-line no-magic-numbers
+                        if (event.keyCode === 8) {
+                            //console.log("DELETE");
                             this.CREATOR_ING_INPUT_CONTAINER[i].remove();
                         }
-                    })
+                    });
                 }
             }
         }
 
-
-        this.CREATOR_ING_ADD.addEventListener("click", (event) => {
+        this.CREATOR_ING_ADD.addEventListener("click", () => {
             let newCreatorIngInputContainer = this.createIngredientInputContainer();
-            this.CREATOR_ING_CONTAINER.insertBefore(newCreatorIngInputContainer, this.CREATOR_ING_CONTAINER.lastElementChild)
+            this.CREATOR_ING_CONTAINER.insertBefore(newCreatorIngInputContainer, this.CREATOR_ING_CONTAINER.lastElementChild);
 
-            newCreatorIngInputContainer.addEventListener("click", (event) => {
-                if (this.CREATOR_ING_RESULTS.childElementCount != 0) {
+            newCreatorIngInputContainer.addEventListener("click", () => {
+                if (this.CREATOR_ING_RESULTS.childElementCount !== 0) {
                     newCreatorIngInputContainer.classList.add("extend");
                 } else {
                     newCreatorIngInputContainer.classList.add("focus");
                 }
             });
             document.addEventListener("click", (event) => {
-                if (!newCreatorIngInputContainer.contains(event.target) && event.target.className != "creator-ingredient") {
+                if (!newCreatorIngInputContainer.contains(event.target) && event.target.className !== "creator-ingredient") {
                     if (newCreatorIngInputContainer.classList.contains("focus")) {
                         newCreatorIngInputContainer.classList.remove("focus");
                     } else if (newCreatorIngInputContainer.classList.contains("extend")) {
@@ -226,17 +215,18 @@ class CocktailCreator extends Observable {
             });
 
             newCreatorIngInputContainer.querySelector(".creator-ingredient-input").addEventListener("keydown", (event) => {
-                if (newCreatorIngInputContainer.querySelector(".creator-ingredient-input").value == "" && event.keyCode == 8) {
-                    newCreatorIngInputContainer.remove()
+                // eslint-disable-next-line no-magic-numbers
+                if (newCreatorIngInputContainer.querySelector(".creator-ingredient-input").value === "" && event.keyCode === 8) {
+                    newCreatorIngInputContainer.remove();
                 }
-            })
+            });
 
             /*
             FILL ING RESULTS
             */
             this.fillIngredientList(newCreatorIngInputContainer.querySelector(".creator-ingredient-input-results"), newCreatorIngInputContainer.querySelector(".creator-ingredient-input"));
 
-        })
+        });
 
     }
 
@@ -246,7 +236,7 @@ class CocktailCreator extends Observable {
 
     initializeInstructionInputContainer() {
         for (let i = 0; i < this.CREATOR_INS_INPUT_CONTAINER.length; i++) {
-            this.CREATOR_INS_INPUT_CONTAINER[i].addEventListener("click", (event) => {
+            this.CREATOR_INS_INPUT_CONTAINER[i].addEventListener("click", () => {
                 this.CREATOR_INS_INPUT_CONTAINER[i].classList.add("focus");
             });
 
@@ -256,21 +246,21 @@ class CocktailCreator extends Observable {
                 }
             });
 
-            if (i != 0) {
+            if (i !== 0) {
                 this.CREATOR_INS_INPUT_CONTAINER[i].querySelector(".creator-instructions-input").addEventListener("keydown", (event) => {
-                    if (this.CREATOR_INS_INPUT_CONTAINER[i].querySelector(".creator-instructions-input").value == "" && event.keyCode == 8) {
-                        this.CREATOR_INS_INPUT_CONTAINER[i].remove()
+                    // eslint-disable-next-line no-magic-numbers
+                    if (this.CREATOR_INS_INPUT_CONTAINER[i].querySelector(".creator-instructions-input").value === "" && event.keyCode === 8) {
+                        this.CREATOR_INS_INPUT_CONTAINER[i].remove();
                     }
-                })
+                });
             }
         }
 
-
-        this.CREATOR_INS_ADD.addEventListener("click", (event) => {
+        this.CREATOR_INS_ADD.addEventListener("click", () => {
             let newCreatorInsInputContainer = this.createInstructionsInputContainer();
-            this.CREATOR_INS_CONTAINER.insertBefore(newCreatorInsInputContainer, this.CREATOR_INS_CONTAINER.lastElementChild)
+            this.CREATOR_INS_CONTAINER.insertBefore(newCreatorInsInputContainer, this.CREATOR_INS_CONTAINER.lastElementChild);
 
-            newCreatorInsInputContainer.addEventListener("click", (event) => {
+            newCreatorInsInputContainer.addEventListener("click", () => {
                 newCreatorInsInputContainer.classList.add("focus");
             });
 
@@ -281,11 +271,12 @@ class CocktailCreator extends Observable {
             });
 
             newCreatorInsInputContainer.querySelector(".creator-instructions-input").addEventListener("keydown", (event) => {
-                if (newCreatorInsInputContainer.querySelector(".creator-instructions-input").value == "" && event.keyCode == 8) {
-                    newCreatorInsInputContainer.remove()
+                // eslint-disable-next-line no-magic-numbers
+                if (newCreatorInsInputContainer.querySelector(".creator-instructions-input").value === "" && event.keyCode === 8) {
+                    newCreatorInsInputContainer.remove();
                 }
-            })
-        })
+            });
+        });
     }
 
     /* 
@@ -293,36 +284,37 @@ class CocktailCreator extends Observable {
     */
 
     collectInput() {
-        let name = document.querySelector(".name-input").value;
+        let name = document.querySelector(".name-input").value,
+            image = "NO_IMAGE",
+            tagList = [],
+            tags = document.getElementsByClassName("creator-tag selected"),
+            ingredientList = [],
+            ingredients = document.getElementsByClassName("creator-ingredient-input"),
+            ingredientsQuantity = document.getElementsByClassName("creator-ingredient-quantity-input"),
+            ingredientsUnit = document.getElementsByClassName("creator-ingredient-unit-input"),
+            stepList = [],
+            steps = document.getElementsByClassName("creator-instructions-input"),
+            data;
 
         // TODO: image richtig holen
         //let fileName = document.querySelector(".image-input").value;
-        let image = "NO_IMAGE"
         if (this.file !== undefined) {
             image = this.file.target.files[0];
         }
 
-        let tagList = [];
-        let tags = document.getElementsByClassName("creator-tag selected");
         for (let i = 0; i < tags.length; i++) {
             tagList.push(tags[i].innerHTML);
         }
 
-        let ingredientList = [];
-        let ingredients = document.getElementsByClassName("creator-ingredient-input");
-        let ingredientsQuantity = document.getElementsByClassName("creator-ingredient-quantity-input");
-        let ingredientsUnit = document.getElementsByClassName("creator-ingredient-unit-input");
         for (let i = 0; i < ingredients.length; i++) {
             ingredientList.push({ "name": ingredients[i].value, "amount": ingredientsQuantity[i].value, "unit": ingredientsUnit[i].value });
         }
 
-        let stepList = []
-        let steps = document.getElementsByClassName("creator-instructions-input");
         for (let i = 0; i < steps.length; i++) {
             stepList.push(steps[i].value);
         }
 
-        let data = { name, image, stepList, ingredientList }
+        data = { name, image, stepList, ingredientList };
         data.tags = tagList;
         return data;
     }
@@ -333,19 +325,19 @@ class CocktailCreator extends Observable {
 
         if (this.isValid(data)) {
 
-            let recipe = new Recipe();
+            let recipe = new Recipe(),
+                steps = {},
+                i = 1;
             data.ingredientList.forEach((ingredient) => {
                 recipe.addMainIngredient(ingredient.name, ingredient.amount, ingredient.unit);
-            })
+            });
             data.ingredientList = undefined;
             data.recipe = recipe;
 
-            let steps = {};
-            let i = 1;
             data.stepList.forEach((step) => {
                 steps[i] = step;
                 i += 1;
-            })
+            });
             data.steps = steps;
 
             this.notifyAll(new Event("COCKTAIL_DATA_FOR_USER", data));
@@ -357,21 +349,33 @@ class CocktailCreator extends Observable {
     // perform check if submitted cocktail details are existent+valid
     isValid(data) {
 
-        if (data.name == "") return false;
+        if (data.name === "") {
+            return false;
+        }
 
-        let hasValidIngredient = true;
+        let hasValidIngredient = true,
+            hasValidStep = true;
         data.ingredientList.forEach((ingredient) => {
-            if (ingredient.name == "") hasValidIngredient = false;
-            if (ingredient.amount == "") hasValidIngredient = false;
+            if (ingredient.name === "") {
+                hasValidIngredient = false;
+            }
+            if (ingredient.amount === "") {
+                hasValidIngredient = false;
+            }
             //if (ingredient.unit == "") hasValidIngredient = false;
-        })
-        if (!hasValidIngredient) return false;
+        });
+        if (!hasValidIngredient) {
+            return false;
+        }
 
-        let hasValidStep = true;
         data.stepList.forEach((step) => {
-            if (step == "") hasValidStep = false;
-        })
-        if (!hasValidStep) return false;
+            if (step === "") {
+                hasValidStep = false;
+            }
+        });
+        if (!hasValidStep) {
+            return false;
+        }
 
         return true;
     }
