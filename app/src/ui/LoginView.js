feca217bbf2a7d3dae9a71b900550524f5c73c93
wai wha <87634748+waiwha-project@users.noreply.github.com>
2022-10-07 19:04:14 +0200

    LoginView.js eslint

--- app/src/ui/LoginView.js
+++ app/src/ui/LoginView.js
@@ -2,7 +2,6 @@ import { Observable, Event } from "../utils/Observable.js";
 
 const LOGIN_SECTION_TEMPLATE = document.getElementById("login-modal-template").innerHTML.trim();
 
-
 function createLoginView() {
     let el = document.createElement("div");
     el.innerHTML = LOGIN_SECTION_TEMPLATE;
@@ -13,28 +12,28 @@ function toggleSignUpLogin(el) {
     if (!el.classList.contains("login")) {
         //toggle to Login
         el.querySelector("#login-field-username").style.display = "none";
-        el.querySelector(".login-question").textContent = "Hast du noch keinen Account?"
-        el.querySelector(".login-link").textContent = "Sign Up"
+        el.querySelector(".login-question").textContent = "Hast du noch keinen Account?";
+        el.querySelector(".login-link").textContent = "Sign Up";
 
-        el.querySelector(".signup-button").textContent = "Login"
-        el.classList.add("login")
+        el.querySelector(".signup-button").textContent = "Login";
+        el.classList.add("login");
     } else {
         //toggle to SignUp
         el.querySelector("#login-field-username").style.display = "flex";
-        el.querySelector(".login-question").textContent = "Hast du schon einen Account?"
-        el.querySelector(".login-link").textContent = "Login"
+        el.querySelector(".login-question").textContent = "Hast du schon einen Account?";
+        el.querySelector(".login-link").textContent = "Login";
         
-        el.querySelector(".signup-button").textContent = "Sign Up"
-        el.classList.remove("login")
+        el.querySelector(".signup-button").textContent = "Sign Up";
+        el.classList.remove("login");
     }
 }
 
 function getUserData(el) {
-    let username = undefined;
-    let email = el.querySelector(".login-email-input").value;
-    let password = el.querySelector(".login-password-input").value;
+    let username, // muss nicht als undefined initialisiert werden, ist so schon undefined
+        email = el.querySelector(".login-email-input").value,
+        password = el.querySelector(".login-password-input").value;
 
-    if (el.querySelector(".signup-button").textContent.trim() == "Sign Up") {
+    if (el.querySelector(".signup-button").textContent.trim() === "Sign Up") {
         username = el.querySelector(".login-username-input").value;
     }
 
@@ -49,20 +48,20 @@ class LoginView extends Observable {
     }
 
     initializeLoginView() {
-        this.el.querySelector(".login-link").addEventListener("click", (event) => {
+        this.el.querySelector(".login-link").addEventListener("click", () => {
             toggleSignUpLogin(this.el);
-        })
+        });
         
-        this.el.querySelector(".signup-button").addEventListener("click", (event) => {
+        this.el.querySelector(".signup-button").addEventListener("click", () => {
             let userData = getUserData(this.el);
-            this.notifyAll(new Event("USER_SUBMIT", userData))
-        })
+            this.notifyAll(new Event("USER_SUBMIT", userData));
+        });
 
         document.querySelector("body").addEventListener("click", (event) => {
             if (!this.el.querySelector(".login-container").contains(event.target) && !document.querySelector("header").contains(event.target) && !document.querySelector(".new-recipe-fab").contains(event.target)) {
                 this.removeLoginView();
             }
-        })
+        });
     }
 
     showLoginView() {
@@ -77,4 +76,4 @@ class LoginView extends Observable {
 
 }
 
-export { LoginView }
+export { LoginView };
